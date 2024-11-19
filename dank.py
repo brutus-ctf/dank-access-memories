86bd1acda02b8fcef361e4d4273894f2faf51d69
brutus-ctf <164179421+brutus-ctf@users.noreply.github.com>
2024-11-19 14:53:29 +0000

    Update dank.py

--- dank.py
+++ dank.py
@@ -11,4 +11,23 @@ from cryptography.fernet import Fernet
 
 dank_database = "dank_memories.db"
 
-#FLAGZ{2-R-FLAGZ-Secret-0r-R-th3y-Publ1c-l0l}
+while True:
+  timestamp = datetime.datetime.now().strftime("%A %d %B %Y, %I:%M:%S.%f %p")
+  event = str(uuid.uuid4())
+  teh_file = str(event) + '.jpg'
+  os_command = 'scrot ' + teh_file
+  sub_return = subprocess.run(os_command, shell=True)
+  if sub_return.returncode == 0:
+    file_binary = open(teh_file, 'rb').read()
+    file_base64_utf8_str = base64.b64encode(file_binary).decode('utf-8')
+    dataurl_struct = f'data:image/jpg;base64,{file_base64_utf8_str}'
+    if len(dataurl_struct) > 0:
+      naughty_naughty = "rm -f " + teh_file
+      os.system(naughty_naughty)
+    else:
+      pass
+    lolz_key = Fernet.generate_key()
+    uber_secure = Fernet(lolz_key).encrypt(dataurl_struct.encode())
+
+  else:
+    pass
