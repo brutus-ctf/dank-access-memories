3d5adaffa7e981f166b9469ca176a11a200be221
brutus-ctf <164179421+brutus-ctf@users.noreply.github.com>
2024-11-19 14:50:40 +0000

    Update dank.py

--- dank.py
+++ dank.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-# Code goes here.
+import os
+import subprocess
+import sqlite3
+import uuid
+import datetime
+import base64
+import time
+from cryptography.fernet import Fernet
+
+dank_database = "dank_memories.db"
